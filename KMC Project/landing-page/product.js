53782e91c66279763e3667bcd17f15f07eb90de8
https://github.com/priyakapoor13/kishori-madhav-creations <priyakapoor2200@gmail.com>
2024-11-19 13:02:58 +0530

    first commit

---
+++ KMC Project/landing-page/product.js
@@ -0,0 +1,46 @@
+let cartCount = localStorage.getItem('cartCount') ? parseInt(localStorage.getItem('cartCount')) : 0;
+
+// Function to update the badge display
+const updateCartBadge = () => {
+  document.getElementById('cart-badge').innerText = cartCount;
+};
+
+// Call the function to set the initial value
+updateCartBadge();
+
+const addToCart = (productId) => {
+  cartCount++;
+  localStorage.setItem('cartCount', cartCount); // Save count to local storage
+  updateCartBadge(); // Update the badge display
+  showNotification(productId);
+};
+
+const showNotification = (productId) => {
+  const notification = document.getElementById(productId);
+  notification.innerText = 'Added to cart!';
+  setTimeout(() => {
+    notification.innerText = '';
+  }, 2000);
+};
+
+
+// // Attach event listeners to all add-to-cart buttons
+// document.querySelectorAll('.add-to-cart-btn').forEach(button => {
+//   button.addEventListener('click', function() {
+//     const productId = this.getAttribute('data-product-id');
+//     addToCart(productId);
+//   });
+// });
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+fetch("./database/product.json").then((response) => response.json())
+    .then((json) => console.log(json)
+ );
---
+++ KMC Project/product-pages/script.js
@@ -0,0 +1,4 @@
+document.getElementById('size-select').addEventListener('change', function() {
+    const selectedSize = this.value;
+    console.log('You have selected size: ' + selectedSize);
+});
